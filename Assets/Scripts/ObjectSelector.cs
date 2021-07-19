3c34a6713c21fcc0948ecda0a9af6598391c2faf
cesarleon911 <34731842+cesarleon911@users.noreply.github.com>
2021-07-18 22:11:38 -0500

    protitpo funcional al 99.9

---
+++ Assets/Scripts/ObjectSelector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelector : MonoBehaviour
+{
+    public List<GameObject> personaje;
+
+    private void Start()
+    {
+        personaje.Add(GameObject.Find("1"));
+        personaje.Add(GameObject.Find("2"));
+        personaje.Add(GameObject.Find("3"));
+        personaje.Add(GameObject.Find("4"));
+        personaje.Add(GameObject.Find("5"));
+        personaje.Add(GameObject.Find("6"));
+        personaje.Add(GameObject.Find("7"));
+        personaje.Add(GameObject.Find("8"));
+        personaje.Add(GameObject.Find("9"));
+
+        foreach(GameObject lienzo in personaje) 
+        {
+            lienzo.SetActive(false);
+        }
+
+
+
+        //aqui se supone que se debe de cargar los elementos a la base local 
+
+        //con la cantidad de personajes se activa la opcion de personajes
+        cargarlienzos(3);
+        //luego de que se carga los lienzos con las imagenes de referencia
+
+
+    }
+
+    private void cargarlienzos(int cant)
+    {
+        personaje[cant-1].SetActive(true);
+    }
+
+    
+}
---
+++ Assets/Scripts/Scenectrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenectrl : MonoBehaviour
+{
+
+    public void btn_back_creditos()
+    {
+        SceneManager.LoadScene("carga");
+    }
+
+    public void btn_cerrar_carga()
+    {
+        SceneManager.LoadScene("espol");
+    }
+
+    public void btn_info_carga()
+    {
+        SceneManager.LoadScene("creditos");
+    }
+
+    public void btn_play()
+    {
+        SceneManager.LoadScene("MenuPersonajes");
+    }
+
+    public void btn_back_versiones() {
+        SceneManager.LoadScene("MenuPersonajes");
+    }
+
+    public void btn_back_pintado() {
+        SceneManager.LoadScene("MenuVersionesPersonajes");
+    }
+
+  
+}
