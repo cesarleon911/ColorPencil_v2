a2dbc948b6b8ab885d09bd5344aea9a2bdb4ad8b
cesarleon911 <34731842+cesarleon911@users.noreply.github.com>
2021-09-13 23:00:02 -0500

    agregar audio

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public static AudioScript instance;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
